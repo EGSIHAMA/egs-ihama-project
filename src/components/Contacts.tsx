365436b82ec7fc3e87db44aaea92a5fd18e8865a
EGSIHAMA <137067923+EGSIHAMA@users.noreply.github.com>
2025-04-27 14:21:28 +0100

    Mobile browser compatibility fix

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <section className="w-full py-20">
+    <section className="w-full bg-gray-100 py-20">
       <div className="max-w-5xl mx-auto px-4 z-11">
         
         <h2 className="text-3xl font-bold mb-12 text-center">Contact Us</h2>
