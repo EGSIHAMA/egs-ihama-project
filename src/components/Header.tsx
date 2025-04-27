aa9cae516cd4d4bed1fa320b58131f39a543c403
EGSIHAMA <137067923+EGSIHAMA@users.noreply.github.com>
2025-04-27 13:27:32 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ interface HeaderProps {
 
 export default function Header({title}: HeaderProps) {
   return (
-    <header className="w-full p-4 bg-gray-800 text-white text-center">
-      <h1 className="text-3xl font-semibold">{title}</h1>
+    <header className="w-full p-4 bg-gray-800 text-gray-800 text-center">
+      <h1 className="text-3xl font-semibold text-gray-800">{title}</h1>
     </header>
   );
 }
